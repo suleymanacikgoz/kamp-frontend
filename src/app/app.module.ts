50042b12cb2c2603c22e48b164b6c88ce4c85a46
suleymanacikgoz <62662812+suleymanacikgoz@users.noreply.github.com>
2021-03-26 15:28:45 +0300

    24 mart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+
+import {ToastrModule} from "ngx-toastr";
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthInterceptor } from './intercaptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,9 +24,26 @@ import { NaviComponent } from './components/navi/navi.component';
     ProductComponent,
     CategoryComponent,
     NaviComponent,
+    VatAddedPipe,
+    FilterPipePipe,
+    CartSummaryComponent,
+    ProductAddComponent,
+    LoginComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
+  ],
+  providers: [
+  {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true}
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,31 +5,37 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-  categories : Category[]=[];
-  currentCategory:Category;
-  constructor(private categoryService:CategoryService) { }
+  categories: Category[] = [];
+  currentCategory: Category;
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.getCategories();
   }
 
   getCategories() {
-    this.categoryService.getCategories().subscribe(response=>{
-      this.categories=response.data
-      
-    })
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+    });
   }
-  setCurrentCategory(category:Category){
-      this.currentCategory = category;
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
-  getCurrentCategoryClass(category:Category){
-    if(category ==this.currentCategory){
-      return "list-group-item active "
-    }else{
-      return "list-group-item "
+  }
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
     }
   }
 }
